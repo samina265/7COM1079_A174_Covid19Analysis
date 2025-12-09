ccdd11f6d11d0d5eac08f4afb7b9f4f07a5745e6
Syeda Samina Hussain <83365244+samina265@users.noreply.github.com>
2025-12-09 15:34:48 +0600

    Added initial data  null value check  and dataset summary

--- code/Analysis.R
+++ code/Analysis.R
@@ -2,4 +2,12 @@
 
 data <- read.csv("C:/Users/samina/Downloads/cases_malaysia_clean.csv")
 
-print(data)
+# print(data)
+
+# null value checked
+sum(is.na(data))
+
+# any missing values in columns
+colSums(is.na(data))
+
+summary(data)
