de1e9ee7ffe743b64f0c104166a7372ad6422a19
Syeda Samina Hussain <83365244+samina265@users.noreply.github.com>
2025-12-10 22:55:51 +0600

    Added histograms for daily new cases and deaths.

--- code/Analysis.R
+++ code/Analysis.R
@@ -1,6 +1,8 @@
 # Load and  Read the two files 
-cases_file  <- "C:/Users/samina/Downloads/1data/cases_malaysia_clean.csv"   
-deaths_file <- "C:/Users/samina/Downloads/1data/deaths_malaysia.csv"     
+
+cases_file  <- "C:/Users/sajjad/Downloads/1data/cases_malaysia_clean.csv"
+deaths_file <- "C:/Users/sajjad/Downloads/1data/deaths_malaysia.csv"
+
 
 cases  <- read.csv(cases_file)
 deaths <- read.csv(deaths_file)
@@ -14,17 +16,26 @@ deaths$date <- as.Date(deaths$date)
 cases_clean  <- cases[, c("date", "cases_new")]
 deaths_clean <- deaths[, c("date", "deaths_new")]
 
-
 # Check columns
-print(names(cases_small))
-print(names(deaths_small))
-
+print(names(cases_clean))
+print(names(deaths_clean))
 
 # Merge by date
-merged <- merge(cases_small, deaths_small, by = "date", all = FALSE)
-
+merged <- merge(cases_clean, deaths_clean, by = "date", all = FALSE)
 
 # remove rows which has null values 
 merged <- merged[!is.na(merged$cases_new) & !is.na(merged$deaths_new), ]
 
-summary(merged)
+#summary(merged)
+
+# Histogram: daily new cases
+hist(merged$cases_new,
+     main = "Histogram of Daily New Cases",
+     xlab = "Daily New Cases",
+     col = "skyblue")
+
+# Histogram: daily new deaths
+hist(merged$deaths_new,
+     main = "Histogram of Daily New Deaths",
+     xlab = "Daily New Deaths",
+     col = "tomato")
